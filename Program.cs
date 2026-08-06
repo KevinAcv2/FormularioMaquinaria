4c6e83729df4f77b4aa83e592fcb2f2b70a9502f
KevinAcv2 <kevin_aacevedo@soy.sena.edu.co>
2026-08-06 10:58:17 -0500

    Cabios en el program

--- Program.cs
+++ Program.cs
@@ -2,16 +2,42 @@ using Maquinarias.Data;
 using Microsoft.EntityFrameworkCore;
 using Maquinarias.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Puerto dinámico para Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllersWithViews();
 
+// Conversión de DATABASE_URL (formato Render) a formato Npgsql
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string connectionString;
+
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={uri.Host};" +
+        $"Port={(uri.Port == -1 ? 5432 : uri.Port)};" +
+        $"Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        $"SSL Mode=Require;Trust Server Certificate=true";
+}
+else
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddSession();
 builder.Services.AddHttpClient<OcrService>();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -22,10 +48,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseSession();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
